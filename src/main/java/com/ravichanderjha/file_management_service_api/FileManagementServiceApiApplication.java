0f8c2db582f1c6dd9fd6c65eb27d950f0145890a
Ravi Chander Jha <ravichanderjha@gmail.com>
2025-05-13 14:50:03 +0530

    INIT

---
+++ src/main/java/com/ravichanderjha/file_management_service_api/FileManagementServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.ravichanderjha.file_management_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagementServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagementServiceApiApplication.class, args);
+	}
+
+}
